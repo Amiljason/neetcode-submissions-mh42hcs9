cba9f7a535bba7ea0714be0e9b8fe9b769b6a2a6
PES2UG23CS117(BANGI AMIL JASON ) <153606611+Amiljason@users.noreply.github.com>
2026-08-05 20:08:45 +0530

    Add: rotate-matrix - submission-0

---
+++ Data Structures & Algorithms/rotate-matrix/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void transpose(int[][] matrix){
+        int r=matrix.length;
+        int c=matrix[0].length;
+        for(int i=0;i<r;i++){
+            for(int j=i+1;j<c;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+    }
+    public void rotate(int[][] matrix) {
+        int row=matrix.length;
+        int col=matrix[0].length;
+        transpose(matrix);
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][col-j-1];
+                matrix[i][col-j-1]=temp;
+            }
+        }
+    }
+}
