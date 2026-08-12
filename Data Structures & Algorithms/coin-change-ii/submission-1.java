c40e8032bf36731ef7c1480704409db7738ecb5f
PES2UG23CS117(BANGI AMIL JASON ) <153606611+Amiljason@users.noreply.github.com>
2026-08-12 09:53:42 +0530

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+     public int countWays(int[] coins, int s, int n){
+      int[][] dp = new int[n+1][s+1];
+      for(int i=0;i<=n;i++) dp[i][0]=1;
+      //for(int i=0;i<=n;i++) dp[i][0]=1;
+      for(int i=1;i<=n;i++){
+        for(int j=1;j<=s;j++){
+            dp[i][j] = dp[i-1][j];
+            if(coins[i-1]<=j) dp[i][j]=dp[i-1][j]+dp[i][j-coins[i-1]]; 
+        }
+      }
+      return dp[n][s];
+    }
+    public int change(int amount, int[] coins) {
+         int n=coins.length;
+        return countWays(coins, amount, n);
+    }
+}
