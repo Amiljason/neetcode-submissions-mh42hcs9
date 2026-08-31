d3fd9f3ae45a075c2dfed4fc79fa9b2e84c768b8
PES2UG23CS117(BANGI AMIL JASON ) <153606611+Amiljason@users.noreply.github.com>
2026-08-31 18:57:20 +0530

    Add: partition-equal-subset-sum - submission-3

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int total=0;
+        for(int n:nums){
+            total+=n;
+        }
+        if(total%2!=0) return false;
+        int target=total/2;
+        boolean[] dp=new boolean[target+1];
+        dp[0]=true;
+        for(int n:nums){
+            for(int sum=target;sum>=n;sum--){
+                dp[sum]=dp[sum]||dp[sum-n];
+            }
+        }
+        return dp[target];
+    }
+}
