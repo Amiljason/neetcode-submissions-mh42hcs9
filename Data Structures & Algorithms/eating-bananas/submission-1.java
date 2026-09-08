3c2ba0b707c658f67acece8920dc62a91d017c67
PES2UG23CS117(BANGI AMIL JASON ) <153606611+Amiljason@users.noreply.github.com>
2026-09-08 14:47:20 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        Arrays.sort(piles);
+        int n=piles.length;
+        int low=1,high=piles[n-1];
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            long hours=0;
+            for(int i=0;i<n;i++){
+                hours+=(piles[i]+mid-1)/mid;
+            }
+            if(hours<=h){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+}
