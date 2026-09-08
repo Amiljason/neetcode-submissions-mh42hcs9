8c6caf836815c5dfe5a9211de375daaa71b4a5e1
PES2UG23CS117(BANGI AMIL JASON ) <153606611+Amiljason@users.noreply.github.com>
2026-09-08 12:35:19 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int n:nums){
+            map.put(n,map.getOrDefault(n,0)+1);
+        }
+        for(Map.Entry<Integer,Integer> e: map.entrySet()){
+            if(e.getValue()>1) return true;
+        }
+        return false;
+    }
+}
